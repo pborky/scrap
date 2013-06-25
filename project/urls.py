f7ff1824162b766ce1939efd1bcd2821a4bacc63
Peter Boráros <pborky@pborky.sk>
2013-06-26 01:34:22 +0200

    adding demo pages

--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^project/', include('project.foo.urls')),
+    # Home Page -- Replace as you prefer
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
